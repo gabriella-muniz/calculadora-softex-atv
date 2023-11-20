6d2afe95ce1adb547e5de45f42e724694298badf
Gabriella Muniz <gabriellamunizzz08@gmail.com>
2023-11-20 14:43:36 -0300

    atividade- function-joyclass

---
+++ atv-function.js
@@ -0,0 +1,22 @@
+//função tardicional
+function oi(){
+    console.log("Olá, Mundo!")
+}
+
+//função tradicional com parâmetros e retorno de valor
+function operacao(a, b){
+    return a + b;
+}
+
+//arrow function 
+const soma= (a, b) => a + b;
+
+//forma logica
+
+let numero1= 7;
+let numero2= 8;
+
+const resultado= soma(numero1, numero2)
+console.log( "O resultado é " + resultado);
+
+
